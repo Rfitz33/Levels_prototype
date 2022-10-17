eeeaa3d72e6e250995e0a814f438ef2750da0931
Rfitz33 <reidfitzgerald@yahoo.com>
2022-10-16 23:07:10 -0500

    Functioning Menu, Settings and End screens as well as images used in these pages

--- app/src/main/java/com/example/levels_prototype/MainActivity.java
+++ app/src/main/java/com/example/levels_prototype/MainActivity.java
@@ -3,17 +3,40 @@ package com.example.levels_prototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable torchFlameAnimationR;
+    AnimationDrawable torchFlameAnimationL;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        torchFlameAnimationL.start();
+        torchFlameAnimationR.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+        ImageView torchImage = (ImageView) findViewById(R.id.torch_image);
+        ImageView torchImage2 = (ImageView) findViewById(R.id.torch_image2);
+        torchImage.setBackgroundResource(R.drawable.torch_animation);
+        torchFlameAnimationL = (AnimationDrawable) torchImage.getBackground();
+        torchImage2.setBackgroundResource(R.drawable.torch_animation);
+        torchFlameAnimationR = (AnimationDrawable) torchImage2.getBackground();
+
+        torchImage.setOnClickListener(view -> torchFlameAnimationL.start());
+        torchImage2.setOnClickListener(view -> torchFlameAnimationR.start());
+    }
     public void SettingsScreen (View view) {
 
         Intent intent = new Intent(this, SettingsScreen.class);
@@ -31,4 +54,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CharacterScreen.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/levels_prototype/SettingsScreen.java
+++ app/src/main/java/com/example/levels_prototype/SettingsScreen.java
@@ -3,15 +3,31 @@ package com.example.levels_prototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class SettingsScreen extends AppCompatActivity {
+
+    AnimationDrawable characterWalkAnimation;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        characterWalkAnimation.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
+
+
+        ImageView characterWalk = (ImageView) findViewById(R.id.characterWalk);
+        characterWalk.setBackgroundResource(R.drawable.character_animation);
+        characterWalkAnimation = (AnimationDrawable) characterWalk.getBackground();
     }
 
     public void NewGameStart (View view) {
